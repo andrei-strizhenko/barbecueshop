940b66a281df6c266e5ae27339642068bd786bf3
андрей стриженко <xparazite1@gmail.com>
2023-09-21 14:21:20 +0300

    deploy REST10

--- src/main/java/com/diplomproject/barbecueshop/BarbecueshopApplication.java
+++ src/main/java/com/diplomproject/barbecueshop/BarbecueshopApplication.java
@@ -13,7 +13,7 @@ public class BarbecueshopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BarbecueshopApplication.class, args);
-        log.info("Swagger-ui run on: http://localhost:9092/swagger-ui/index.html");
+        log.info("Swagger-ui run on: http://https://dashboard.render.com/web/srv-ck60rq8s0i2c73bq7ac0/deploys/dep-ck60rqos0i2c73bq7k4g/swagger-ui/index.html");
     }
 
 }
